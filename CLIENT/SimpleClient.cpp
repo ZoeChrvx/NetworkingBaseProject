aa778db2c5e8d9e2961465e9479ac4bfe71a4b70
ZoeChrvx <zchervaux@artfx.fr>
2024-04-29 10:42:08 +0200

    [add] Base for server and client
    Basic message

--- CLIENT/SimpleClient.cpp
+++ CLIENT/SimpleClient.cpp
@@ -11,6 +11,42 @@ int main(int argc, char* argv[]) {
 		return 1;
 	}
 
+	//Trouver la bonne adresse
+	IPaddress ip;
+	if(SDLNet_ResolveHost(&ip, "localhost", 4242)==1)
+	{
+		cerr<< "Resolve Host Error: " << SDLNet_GetError() << endl;
+		SDLNet_Quit();
+		return 1;
+	}
+
+	//Creer la "socket"
+	TCPsocket clientSocket = SDLNet_TCP_Open(&ip);
+	if(!clientSocket)
+	{
+		cerr << "TCP Open error: " << SDLNet_GetError() << endl;
+		SDLNet_Quit();
+		return 1;
+	}
+
+	//Envoyer un message
+	string message = "Hi there";
+
+	int bytesSent = SDLNet_TCP_Send(clientSocket, message.c_str(), message.length()+1);
+	if(bytesSent < message.length()+1)
+	{
+		cerr << "SDLNet TCP Send error: " << SDLNet_GetError()<<endl;
+		SDLNet_TCP_Close(clientSocket);
+		SDLNet_Quit();
+		return 1;
+	}
+
+	cout << "Sent " << bytesSent << " bytes to the server !" << std::endl;
+	SDLNet_TCP_Close(clientSocket);
+	SDLNet_Quit();
+	
+
+	
 	cout << "Thank you for using ChArtFX !\n";
 	return 0;
 }
--- SERVER/SimpleServer.cpp
+++ SERVER/SimpleServer.cpp
@@ -3,13 +3,50 @@
 #include <iostream>
 using namespace std;
 
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[])
+{
+    if (SDLNet_Init() == -1)
+    {
+        cerr << "SDLNet_Init error: " << SDLNet_GetError() << endl;
+        return 1;
+    }
 
-	if (SDLNet_Init() == -1) {
-		cerr << "SDLNet_Init error: " << SDLNet_GetError() << endl;
-		return 1;
-	}
+    //Qui je suis
+    IPaddress ip;
+    if (SDLNet_ResolveHost(&ip, nullptr, 4242) == -1)
+    {
+        cerr << "Resolve Host error: " << SDLNet_GetError() << endl;
+        SDLNet_Quit();
+        return 1;
+    }
 
-	cout << "Thank you for using ChArtFX !\n";
-	return 0;
+    //J'ouvre mes "socket"
+    TCPsocket serverSocket = SDLNet_TCP_Open(&ip);
+    if (!serverSocket)
+    {
+        cerr << "TCP Open error: " << SDLNet_GetError() << endl;
+        SDLNet_Quit();
+        return 1;
+    }
+
+    //J'attend des clients
+    TCPsocket clientSocket;
+    while (true)
+    {
+        clientSocket = SDLNet_TCP_Accept(serverSocket);
+        if(clientSocket)
+        {
+            cout << "A client reached the server !" << endl;
+            char buffer[1024];
+            int bytesRead = SDLNet_TCP_Recv(clientSocket, buffer, sizeof(buffer));
+            if(bytesRead > 0)
+            {
+                cout << "Incoming message: " << buffer << endl;
+                break;
+            }
+        }
+    }
+
+    cout << "Thank you for using ChArtFX !\n";
+    return 0;
 }
