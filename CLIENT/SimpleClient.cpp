6006e94da0980c6df6cbc35d423ca081e5752abb
ZoeChrvx <zchervaux@artfx.fr>
2024-04-30 14:03:28 +0200

    [add] Begin of server

--- CLIENT/SimpleClient.cpp
+++ CLIENT/SimpleClient.cpp
@@ -1,16 +1,127 @@
 #include <SDL.h>
 #include <SDL_net.h>
 #include <iostream>
+#include <raylib.h>
+#include <vector>
 //#include <string>
 using namespace std;
 
+struct Message
+{
+    bool fromMe = false;
+    string content;
+};
+
 int main(int argc, char* argv[]) {
 
-	if (SDLNet_Init() == -1) {
-		cerr << "SDLNet_Init error: " << SDLNet_GetError() << endl;
-		return 1;
-	}
+    if (SDLNet_Init() == -1) {
+        cerr << "SDLNet_Init error: " << SDLNet_GetError() << endl;
+        return 1;
+    }
+    //ip strin,g et port int
+    cout << "Saisissez l'ip." << endl;
+    string ipClient;
+    cin >> ipClient;
+    cout << "Saisissez le port" << endl;
+    int host;
+    cin >> host;
+    cout << "Saisissez votre nom." << endl;
+    string pseudoClient;
+    cin >> pseudoClient;
+    
+    
+    
+    //Trouver la bonne adresse
+    IPaddress ip;
+    if(SDLNet_ResolveHost(&ip, ipClient.c_str(), host)==1)
+    {
+        cerr<< "Resolve Host Error: " << SDLNet_GetError() << endl;
+        SDLNet_Quit();
+        return 1;
+    }
 
-	cout << "Thank you for using ChArtFX !\n";
-	return 0;
+    //Creer la "socket"
+    TCPsocket clientSocket = SDLNet_TCP_Open(&ip);
+    if(!clientSocket)
+    {
+        cerr << "TCP Open error: " << SDLNet_GetError() << endl;
+        SDLNet_Quit();
+        return 1;
+    }
+
+    int pseudoClientSend = SDLNet_TCP_Send(clientSocket, pseudoClient.c_str(), pseudoClient.length()+1);
+
+    vector<Message> log{Message{ false, "Waiting for someone to talk to ..."}};
+    
+    const int width = 500, height = 750;
+    InitWindow(width, height, "My first chat window !");
+    SetTargetFPS(60);
+
+
+    string typing;
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(GRAY);
+        DrawText("Welcome to ChArtFX !", 220, 15, 25, WHITE);
+        DrawRectangle(20,50,width-40,height-150, DARKBLUE);
+        
+        DrawRectangle(20,height-90,width-40,50, LIGHTGRAY);
+
+        int inputChar = GetCharPressed();
+        if(inputChar != 0)
+        {
+            typing += static_cast<char>(inputChar);
+        }
+        if(typing.size()>0)
+        {
+            if(IsKeyPressed(KEY_BACKSPACE))typing.pop_back();
+            else if (IsKeyPressed(KEY_ENTER))
+            {
+                log.push_back(Message{true, typing});
+                int bytesSent = SDLNet_TCP_Send(clientSocket, typing.c_str(), typing.length()+1);
+                if(bytesSent < typing.length()+1)
+                {
+                    cerr << "SDLNet TCP Send error: " << SDLNet_GetError()<<endl;
+                    SDLNet_TCP_Close(clientSocket);
+                    SDLNet_Quit();
+                    return 1;
+                }
+                typing.clear();
+                
+                char buffer[1024];
+                
+                if(!SDLNet_SocketReady(clientSocket)) continue;
+                int bytesRead = SDLNet_TCP_Recv(clientSocket, buffer, sizeof(buffer));
+                if (bytesRead <= 0)
+                {
+                    cerr << "SDLNet TCP Recv error: " << SDLNet_GetError() << endl;
+                    SDLNet_TCP_Close(clientSocket);
+                    SDLNet_Quit();
+                    return 1;
+                }
+                cout << "Sent " << bytesSent << " bytes to the server !" << std::endl;
+                cout << "Incoming response: " << buffer << endl;
+                
+            }
+            for(int msg = 0; msg < log.size(); msg++)
+            {
+                DrawText(log[msg].content.c_str(), 30,90+(msg*30),15, log[msg].fromMe?SKYBLUE:PURPLE);
+            }
+            DrawText(typing.c_str(), 30, height - 75, 25, BLACK);
+        }
+        
+        EndDrawing();
+    }
+
+    //Envoyer un message
+
+    
+    SDLNet_TCP_Close(clientSocket);
+    SDLNet_Quit();
+	
+
+	
+    cout << "Thank you for using ChArtFX !\n";
+    return 0;
 }
--- SERVER/SimpleServer.cpp
+++ SERVER/SimpleServer.cpp
@@ -1,15 +1,89 @@
 #include <SDL.h>
 #include <SDL_net.h>
 #include <iostream>
+#include <map>
+#include <vector>
 using namespace std;
 
-int main(int argc, char* argv[]) {
-
-	if (SDLNet_Init() == -1) {
+int main(int argc, char* argv[])
+{
+	map<IPaddress*, std::string> clientInfo;
+	if (SDLNet_Init() == -1)
+	{
 		cerr << "SDLNet_Init error: " << SDLNet_GetError() << endl;
 		return 1;
 	}
 
+	//Qui je suis
+	IPaddress ip;
+	if (SDLNet_ResolveHost(&ip, nullptr, 4242) == -1)
+	{
+		cerr << "Resolve Host error: " << SDLNet_GetError() << endl;
+		SDLNet_Quit();
+		return 1;
+	}
+
+	//J'ouvre mes "socket"
+	TCPsocket serverSocket = SDLNet_TCP_Open(&ip);
+	if (!serverSocket)
+	{
+		cerr << "TCP Open error: " << SDLNet_GetError() << endl;
+		SDLNet_Quit();
+		return 1;
+	}
+
+	//J'attend des clients
+	SDLNet_SocketSet set = SDLNet_AllocSocketSet(8);
+	vector<TCPsocket*> tcpSockets;
+	while (true)
+	{
+		TCPsocket clientSocket;
+
+		clientSocket = SDLNet_TCP_Accept(serverSocket);
+		if(clientSocket)
+		{
+			cout << "A client reached the server !" << endl;
+			SDLNet_AddSocket(set, reinterpret_cast<SDLNet_GenericSocket>(clientSocket));
+			IPaddress* clientIP = SDLNet_TCP_GetPeerAddress(clientSocket);
+			char buffer[1024];
+			int bytesRead = SDLNet_TCP_Recv(clientSocket, buffer, sizeof(buffer));
+			if(bytesRead > 0 )//&& SDLNet_CheckSockets(set, 0)!=0)
+			{
+				if(clientInfo.find(clientIP) == clientInfo.end())
+				{
+					clientInfo.try_emplace(clientIP, buffer);
+					tcpSockets.emplace_back(&clientSocket);
+				}
+				cout << "Pseudo: " << buffer << endl;
+				
+			}
+		}
+		if(clientInfo.size() > 0 && SDLNet_CheckSockets(set, 0)!=0)
+		{
+			char buffer[2048];
+			for(TCPsocket* sock : tcpSockets)
+			{
+				if(!SDLNet_SocketReady(sock))
+				{
+					continue;
+				}
+				IPaddress* sockIP = SDLNet_TCP_GetPeerAddress(*reinterpret_cast<TCPsocket*>(sock));
+				int bytesRead = SDLNet_TCP_Recv(clientSocket, buffer, sizeof(buffer));
+				if(bytesRead > 0)
+				{
+					cout << "Incoming message from"<<clientInfo[sockIP]<<": " << buffer << endl;
+				}
+			}
+			
+		}
+	}
+
 	cout << "Thank you for using ChArtFX !\n";
+	for(TCPsocket* sock: tcpSockets)
+	{
+		SDLNet_TCP_Close(*sock);
+	}
+	//SDLNet_TCP_Close(set);
+	SDLNet_Quit();
 	return 0;
 }
